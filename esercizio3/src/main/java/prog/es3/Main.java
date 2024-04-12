7b5d0a1134a426f47e8ef520c090aaee2a8dfd3b
lai2-24 <lai2-24@itismeucci.com>
2024-04-12 12:44:55 +0200

    femmine

--- esercizio3/src/main/java/prog/es3/Main.java
+++ esercizio3/src/main/java/prog/es3/Main.java
@@ -3,14 +3,12 @@ package prog.es3;
 public class Main {
     public static void main(String[] args) {
         Bagno b1 = new Bagno();
-        String[] nomi = {"Marco", "Marta", "Francesco","Lorenzo"};
-        ThreadB t = new ThreadB(b1, nomi[0], "m");
-        ThreadB t1 = new ThreadB(b1, nomi[1], "f");
-        ThreadB t2 = new ThreadB(b1, nomi[2], "m");
-        ThreadB t3 = new ThreadB(b1, nomi[3], "m");
-        t.start();
-        t1.start();
-        t2.start();
-        t3.start();
+        String[] nomi = {"Marco", "Marta", "Francesco","Lorenzo", "Maria"};
+        String[] generi = {"m", "f", "m","m", "f"};
+        
+        for(int i=0; i<nomi.length;i++){
+            ThreadB t = new ThreadB(b1, nomi[i], generi[i]);
+            t.start();
+        }
     }
 }
