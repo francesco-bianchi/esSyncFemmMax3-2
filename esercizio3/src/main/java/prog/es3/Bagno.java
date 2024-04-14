272ec8598ae40e6691043b6dbf6ba60c11608e54
francesco-bianchi <bianchi.francesco@itismeucci.com>
2024-04-14 19:19:34 +0200

    modifica max 3 donne

--- esercizio3/src/main/java/prog/es3/Bagno.java
+++ esercizio3/src/main/java/prog/es3/Bagno.java
@@ -5,33 +5,41 @@ public class Bagno {
     private boolean disponibile=true;
     private int contaD=0;
     private int contaEntr=0;
-    private int x=0;
+    private boolean entrPossD=true;
 
     public synchronized void entra(ThreadB t){
         String name = Thread.currentThread().getName();
         if(t.getGenere().equals("f")){
             contaD++;
-            contaEntr++;
         }
         try {
-            while(!disponibile || contaD>0 && t.getGenere().equals("m") && contaEntr==x+3){
+            while(!disponibile || (contaD>0 && t.getGenere().equals("m") && entrPossD==true) ||  (t.getGenere().equals("f") && entrPossD==false)){
                 wait();
-                System.out.println(name +" è in attesa di entrare");
+                //System.out.println(name +" è in attesa di entrare");
             }
         }
         catch (InterruptedException e) {
             e.printStackTrace();
         }
         disponibile=false;
-        if(t.getGenere().equals("f")) contaD--;
-        System.out.println(name + " e' entrato in bagno");
+        if(t.getGenere().equals("f")){
+            contaD--;
+            contaEntr++;
+            if(contaEntr==3)
+                entrPossD=false;
+        } 
+        else{
+            entrPossD=true;
+            contaEntr=0;
+        }
+        System.out.println(name + " è in bagno");
     }
 
     public synchronized void esci(){
         String name = Thread.currentThread().getName();
         disponibile=true;
         notifyAll();
-        System.out.println(name + " e' uscito dal bagno");        
+        System.out.println(name + " ha lasciato il bagno");        
     }
 
 }
--- esercizio3/src/main/java/prog/es3/Main.java
+++ esercizio3/src/main/java/prog/es3/Main.java
@@ -3,8 +3,8 @@ package prog.es3;
 public class Main {
     public static void main(String[] args) {
         Bagno b1 = new Bagno();
-        String[] nomi = {"Marco", "Marta", "Francesco","Lorenzo", "Maria"};
-        String[] generi = {"m", "f", "m","m", "f"};
+        String[] nomi = {"Marco", "Marta", "Martina", "Francesca","Lorenzo", "Maria", "Angela", "Franca", "Gianna", "Anna", "Carlo",  "Antonio"};
+        String[] generi = {"m", "f", "f", "f", "m", "f", "f", "f", "f", "f", "m", "m"};
         
         for(int i=0; i<nomi.length;i++){
             ThreadB t = new ThreadB(b1, nomi[i], generi[i]);
